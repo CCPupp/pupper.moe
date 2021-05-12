ee1cdacdaa8c5ce6899b75650761449c8adda896
CCPupp <cjerskine@gmail.com>
2021-05-12 17:24:35 -0400

    discord fix

--- internal/discord/discord.go
+++ internal/discord/discord.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/signal"
 	"strconv"
 	"strings"
+	"syscall"
 
 	"secret"
 
@@ -67,6 +69,15 @@ func StartBot() {
 		fmt.Println("error opening connection,", err)
 		return
 	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
 
 }
 
