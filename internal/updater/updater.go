bf75f7708f87464d031dc06ea73ec3d47c5f5190
CCPupp <cjerskine@gmail.com>
2021-08-10 19:51:57 -0400

    fix

--- internal/updater/updater.go
+++ internal/updater/updater.go
@@ -18,7 +18,7 @@ func StartUpdate() {
 
 func updateLoop() {
 	for {
-		go doUpdate()
+		doUpdate()
 		time.Sleep(8 * time.Hour)
 	}
 }
@@ -41,7 +41,7 @@ func updateUser(users []player.User, i int, clientToken string) {
 
 func backupLoop() {
 	for {
-		go backup()
+		backup()
 		time.Sleep(30 * time.Minute)
 	}
 }
